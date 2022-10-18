376f5e75cad24b6e8e56266754bce1ed19a325e9
yelee12 <111097613+yelee12@users.noreply.github.com>
2022-10-18 18:50:50 +0900

    boolPlaceCan't

--- Assets/LYD/Script/Build.cs
+++ Assets/LYD/Script/Build.cs
@@ -5,42 +5,49 @@ using UnityEngine;
 //배치된 거 삭제하기
 public class Build : MonoBehaviour
 {
-
+    /*GameObject place;
    public enum State
     {
         Add,
         Remove
     }
-    public State state;
+    public State state;*/
 
     static public GameObject furniture;
     static public GameObject previewObject;
 
-    public GameObject removeImage;
+    //public GameObject removeImage;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+                //place.GetComponent<PreviewObject>().placeOb = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(state == State.Add)
-       {
+       //if(state == State.Add)
+       //{
+           // place.GetComponent<PreviewObject>().Wall();
+            //place.GetComponent<PreviewObject>().Lamp();
             //마우스 왼쪽 버튼을 클릭하면
             if (Input.GetMouseButtonDown(0))
             {
+                //place.GetComponent<PreviewObject>().placeOb = true;
+
                 GameObject objectClone = Instantiate(furniture, previewObject.transform.position, previewObject.transform.rotation);
                 //*wall.transform.position = transform.position;
 
             }
-       }
+       //}
        
        
-        if(state == State.Remove)
+       /* if(state == State.Remove)
         {
+            //previewObject에 있는 bool 값이 false로 바꿔줘야한다.
+            place.GetComponent<PreviewObject>().placeOb = false;
             //ray를 쏜다.
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -56,19 +63,19 @@ public class Build : MonoBehaviour
 
 
             }
-        }
+        }*/
        
 
     }
 
     //remove 버튼(UI)을 누른다.
 
-    public void OnObjectRemove()
+   /* public void OnObjectRemove()
     {
         state = State.Remove;
         //상태를 제거상태로 바꾼다.
         
-    }
+    }*/
     
 
 
--- Assets/LYD/Script/PreviewObject.cs
+++ Assets/LYD/Script/PreviewObject.cs
@@ -12,34 +12,45 @@ public class PreviewObject : MonoBehaviour
     public GameObject previewLamp;
 
     GameObject preview;
+
+    
+   // public bool placeOb = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+       // placeOb = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Build.previewObject = preview;
-        //마우스의 위치에 따라 레이를 쏜다.
-        Vector3 mouse = Input.mousePosition;
-        Ray casepoint = Camera.main.ScreenPointToRay(mouse);
-        RaycastHit hit;
-        if(Physics.Raycast(casepoint, out hit, Mathf.Infinity) && preview)
-        {
-            preview.transform.position = new Vector3(hit.point.x, hit.point.y + preview.transform.localScale.y / 2, hit.point.z);
-        }
-        
+        //만약 placeOb가 Add 일때 true이고, remove일때 false로 만들어준다. 오브젝트가 보이면 안된다. 
+        //오브젝트가 마우스에 따라 움직이는 것은 여기 스크립트에서 움직인다. 
+        //if(placeOb)
+        //{
+            Build.previewObject = preview;
+            //마우스의 위치에 따라 레이를 쏜다.
+            Vector3 mouse = Input.mousePosition;
+            Ray casepoint = Camera.main.ScreenPointToRay(mouse);
+            RaycastHit hit;
+            if (Physics.Raycast(casepoint, out hit, Mathf.Infinity) && preview)
+            {
+                //오브젝트가 마우스에 따라 움직임
+                preview.transform.position = new Vector3(hit.point.x, hit.point.y + preview.transform.localScale.y / 2, hit.point.z);
+            }
 
-        if(Input.GetMouseButtonDown(1))
-        {
-            preview.transform.Rotate(0, 90, 0);
-        }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                preview.transform.Rotate(0, 90, 0);
+            }
+        //}
+        
     }
     
     public void Wall()
     {
+        //placeOb = true;
         preview = previewWall;
         preview.SetActive(true);
         previewLamp.SetActive(false);
@@ -48,6 +59,7 @@ public class PreviewObject : MonoBehaviour
 
     public void Lamp()
     {
+        //placeOb = true;
         preview = previewLamp;
         preview.SetActive(true);
         previewWall.SetActive(false);
